3775462a3c1be486a11da27582831fcbdb31f71f
Aziangelo <aziangelo@gmail.com>
2024-06-25 15:01:14 +0800

    Win Test (time)

--- include/azify/utils/time.glsl
+++ include/azify/utils/time.glsl
@@ -3,7 +3,7 @@
  
 // TIME AND WORLD DETECTIONS
 #define detect(a,b,x) clamp(((x)-(a))/((b)-(a)), 0.0, 1.0)
-float AFrain  = detect(0.66, 0.3, FogAndDistanceControl.x);
+#define AFrain detect(0.66, 0.3, FogAndDistanceControl.x)
 float AFnight = mix( detect( 0.65, 0.02, FogColor.r ), detect( 0.15, 0.01, FogColor.g ), AFrain );
 float AFdusk  = mix( detect( 1.0, 0.0, FogColor.b ), detect( 0.25, 0.15, FogColor.g ), AFrain );
 float AFday   = detect(0.02, 0.65, FogColor.r);
