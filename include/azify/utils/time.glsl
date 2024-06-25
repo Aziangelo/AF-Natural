c98364c9a2a29c34816de7b88e61d3f71da1de20
Aziangelo <aziangelo@gmail.com>
2024-06-25 14:54:30 +0800

    Win Test Start

--- include/azify/utils/time.glsl
+++ include/azify/utils/time.glsl
@@ -3,10 +3,10 @@
  
 // TIME AND WORLD DETECTIONS
 #define detect(a,b,x) clamp(((x)-(a))/((b)-(a)), 0.0, 1.0)
-const float AFrain  = smoothstep(0.66, 0.3, FogAndDistanceControl.x);
-const float AFnight = mix( detect( 0.65, 0.02, FogColor.r ), detect( 0.15, 0.01, FogColor.g ), AFrain );
-const float AFdusk  = mix( detect( 1.0, 0.0, FogColor.b ), detect( 0.25, 0.15, FogColor.g ), AFrain );
-const float AFday   = detect(0.02, 0.65, FogColor.r);
+float AFrain  = detect(0.66, 0.3, FogAndDistanceControl.x);
+float AFnight = mix( detect( 0.65, 0.02, FogColor.r ), detect( 0.15, 0.01, FogColor.g ), AFrain );
+float AFdusk  = mix( detect( 1.0, 0.0, FogColor.b ), detect( 0.25, 0.15, FogColor.g ), AFrain );
+float AFday   = detect(0.02, 0.65, FogColor.r);
 #define timecycle( a, b, c ) mix( mix( a, b, AFdusk ), c, AFnight )
 vec4 wtime = vec4(AFday,AFdusk,AFnight,AFrain);
  
